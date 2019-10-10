cb4b6257660386faf87be79db1e8bf6745695352
fionicholas <fiorentcheaters@gmail.com>
2019-10-10 10:49:17 +0700

    Initialize Project Sukabaca

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.penulis')
+@section('content')
+<div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title">Dashboard</h4>
+              <p class="card-category">Penulis</p>
+            </div>
+            <div class="card-body">
+              <div id="typography">
+                <div class="card-title">
+                  <h2>Dashboard</h2>
+                </div>
+                <div class="row">
+                  <div class="tim-typo">
+                    
+                  </div>
+                
+                </div>
+              </div>
+            </div>
+          </div>
+@endsection
